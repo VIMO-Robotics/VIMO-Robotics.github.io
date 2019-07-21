866f8ca20f404c178314de6575fe0b1768d67fff
championway <cpwearth.eed03@g2.nctu.edu.tw>
2019-07-21 14:14:15 +0800

    scroll down function

--- js/main.js
+++ js/main.js
@@ -154,6 +154,20 @@ $(document).ready(function(){
         }
 
     });
+	
+	// Page scroll
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
+        }
+    });
   
 
   // Header scroll class
